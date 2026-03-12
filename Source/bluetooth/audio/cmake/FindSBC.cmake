e28d479bea56b07cff4c246ce4974bdeb18a5cf9
sebaszm <s.szczepaniak@metrological.com>
2026-03-12 10:04:03 +0100

    Move Bluetooth and Broadcast

--- Source/bluetooth/audio/cmake/FindSBC.cmake
+++ Source/bluetooth/audio/cmake/FindSBC.cmake
@@ -1,7 +1,7 @@
-# If not stated otherwise in this file or this component's license file the
+# If not stated otherwise in this file or this component's LICENSE file the
 # following copyright and licenses apply:
 #
-# Copyright 2020 Metrological
+# Copyright 2021 Metrological
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,21 +15,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-option(ASCONNECTOR
-        "Include the asconnector library." OFF)
-option(BLUETOOTH
-        "Include bluetooth library in the build." OFF)
-option(BROADCAST
-        "Include broadcasting library in the build." OFF)
+# - Try to find sbc
+# Once done this will define
+#  SBC_FOUND - System has libsbc
+#  SBC_INCLUDE_DIRS - The libsbc include directories
+#  SBC_LIBRARIES - The libraries needed to use libsbc
 
-if(ASCONNECTOR)
-    add_subdirectory(asconnector)
-endif()
+find_package(PkgConfig)
+pkg_check_modules(SBC REQUIRED sbc IMPORTED_TARGET)
 
-if(BLUETOOTH)
-    add_subdirectory(bluetooth)
-endif()
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(SBC DEFAULT_MSG SBC_LIBRARIES)
 
-if(BROADCAST)
-    add_subdirectory(broadcast)
+mark_as_advanced(SBC_FOUND SBC_LIBRARIES)
+
+if(SBC_FOUND)
+   add_library(SBC::SBC ALIAS PkgConfig::SBC)
 endif()
